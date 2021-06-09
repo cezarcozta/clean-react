f9e15b32567354c7e403a88bc1f664411a51611c
cezarcozta <cezarcozta@gmail.com>
2021-06-09 11:08:02 -0300

    feat: ensure remote auth return accountModel on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -5,22 +5,22 @@ import { NotFoundError } from '../../../domain/errors/not-found-error';
 import { ServerError } from '../../../domain/errors/server-error';
 import { UnexpectedError } from '../../../domain/errors/unexpected-error';
 import { AccountModel } from '../../../domain/models/account-model';
-import { LogInCredentials } from '../../../domain/usecases/authentication';
+import { IAuthentication, LogInCredentials } from '../../../domain/usecases/authentication';
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements IAuthentication{
    constructor(
       private readonly url: string, 
       private readonly httpPostClient: HttpPostClient<LogInCredentials, AccountModel>
    ){}
 
-   async auth(params: LogInCredentials): Promise<void>{
+   async auth(params: LogInCredentials): Promise<AccountModel>{
       const httpResponse = await this.httpPostClient.post({
          url: this.url,
          body: params,
       })
 
       switch(httpResponse.statusCode){
-         case EHttpStatusCode.ok: break;
+         case EHttpStatusCode.ok: return httpResponse.body;
          case EHttpStatusCode.unauthorized: throw new InvalidCredentialsError()
          case EHttpStatusCode.notFound: throw new NotFoundError()
          case EHttpStatusCode.serverError: throw new ServerError()
