682f64fd22ca41b14673f89446244dfe73f552c5
cezarcozta <cezarcozta@gmail.com>
2021-06-10 16:34:26 -0300

    feat: root route

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -2,5 +2,5 @@ export { default as Footer } from './footer/footer'
 export { FormStatus } from './form-status/form-status'
 export { Input } from './input/input'
 export { default as LoginHeader } from './login-header/login-header'
+export { Router } from './router/router'
 export { Spinner } from './spinner/spinner'
-
