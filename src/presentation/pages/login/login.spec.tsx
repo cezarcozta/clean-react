3b5b4a1404cc2f99134819f4a5a2aac24609c89d
cezarcozta <cezarcozta@gmail.com>
2021-06-14 10:26:53 -0300

    test: ensure button has disabled initial state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,9 +2,11 @@ import { render } from "@testing-library/react";
 import React from "react";
 import { Login } from "./login";
 describe("Login Component Screen", () => {
-  test("Should not render the spinner and error component on start", () => {
+  test("Should start with inital state", () => {
     const { getByTestId } = render(<Login />);
     const errorWrap = getByTestId("error-wrap");
     expect(errorWrap.childElementCount).toBe(0);
+    const submitButton = getByTestId("submit") as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(0);
   });
 });
